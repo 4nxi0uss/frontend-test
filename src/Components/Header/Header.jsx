7890278190ce38c526a05700503c305e0794e86b
Artur Zaczek <artur200212@interia.pl>
2022-08-19 20:03:35 +0200

    config commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './Header.scss'
+
+class Header extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    state = {}
+
+    render() {
+        return (
+            <header>
+                Hello maj frend
+            </header>
+        );
+    }
+}
+
+export default Header;
