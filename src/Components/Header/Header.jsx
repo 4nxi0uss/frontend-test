0713c7347da1c36b8893c494caf27c9035bc0f51
Artur Zaczek <artur200212@interia.pl>
2022-08-19 22:01:52 +0200

    1 stage of header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 
+import Logo from '../../img/logo-header.svg'
+import Cart from '../../img/cart-empty.svg'
+import ArrowDown from '../../img/arrow-down.svg'
+
 import './Header.scss'
 
 class Header extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
 
     state = {}
 
     render() {
         return (
-            <header>
-                Hello maj frend
+            <header className='header'>
+                <ul className='header__nav'>
+                    <li>WOMEN</li>
+                    <li>MEN</li>
+                    <li>KIDS</li>
+                </ul>
+                <img src={Logo} alt="header logo" className='header__logo' />
+                <div className='header__action'>
+                    <p className='header__action--currency'>PLN <img src={ArrowDown} alt="arrow down" /></p>
+                    <img src={Cart} alt="empty cart" className='header__action--cart' />
+                </div>
+
             </header>
         );
     }
