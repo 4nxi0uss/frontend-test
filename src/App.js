43dee82a57ddc74e7e682f93de89e6ec16854d9f
Artur Zaczek <artur200212@interia.pl>
2022-08-20 13:29:05 +0200

    adding to app component main content component and changing style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
 import './App.scss';
+
 import Header from './Components/Header/Header';
+import MainContent from './Components/MainContent/MainContent';
 
 class App extends Component {
-  state = {}
   render() {
 
     return (
       <div className="App">
         <Header />
+        <MainContent />
       </div>
     );
   }
