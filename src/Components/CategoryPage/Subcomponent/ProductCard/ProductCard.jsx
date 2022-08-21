9f5451f342ab0c3f560392c719c5ce3ac826f085
Artur Zaczek <artur200212@interia.pl>
2022-08-21 15:38:30 +0200

    added fetch data

--- src/Components/CategoryPage/Subcomponent/ProductCard/ProductCard.jsx
+++ src/Components/CategoryPage/Subcomponent/ProductCard/ProductCard.jsx
@@ -2,28 +2,27 @@ import React, { Component } from 'react';
 
 import './ProductCard.scss'
 
-import img from '../../../../img/test-product.png'
-import img2 from '../../../../img/test-product2.png'
 import addToCart from '../../../../img/add-to-cart.svg'
 
 class ProductCard extends Component {
     state = {}
 
     render() {
+        const { name, inStock, gallery, brand, prices } = this.props
 
         const handleAddToCart = (e) => {
             console.log(e)
         }
 
         return (<article className={`product-card `}>
-            <div className={`product-card__div-img ${!this.props.inStock && ' product-card__out-of-stock-img'}`}>
-                <img src={img} alt="img" className={`product-card__div-img__img `} />
-                {!this.props.inStock && <p className={`product-card__div-img__text`}>out of stock</p>}
+            <div className={`product-card__div-img ${!inStock && ' product-card__out-of-stock-img'}`}>
+                <img src={gallery[0]} alt="img" className={`product-card__div-img__img `} />
+                {!inStock && <p className={`product-card__div-img__text`}>out of stock</p>}
 
-                {this.props.inStock && <img src={addToCart} alt="add to cart" className={`product-card__add-to-cart`} onClick={handleAddToCart} />}
+                {inStock && <img src={addToCart} alt="add to cart" className={`product-card__add-to-cart`} onClick={handleAddToCart} />}
             </div>
-            <p className={`product-card__title ${!this.props.inStock && ' product-card__out-of-stock-text'}`}>Apollo Running Short</p>
-            <p className={`product-card__price ${!this.props.inStock && ' product-card__out-of-stock-text'}`}>$50.00</p>
+            <p className={`product-card__title ${!inStock && ' product-card__out-of-stock-text'}`}>{brand} {name}</p>
+            <p className={`product-card__price ${!inStock && ' product-card__out-of-stock-text'}`}>{prices[0].currency.symbol}{prices[0].amount}</p>
         </article>);
     }
 }
