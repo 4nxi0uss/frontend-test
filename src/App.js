2802124242223e658891ed2ecb81d7f1a2199b81
Artur Zaczek <artur200212@interia.pl>
2022-08-21 14:58:51 +0200

    delete opus config

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import './App.scss';
 
 import Header from './Components/Header/Header';
 import MainContent from './Components/MainContent/MainContent';
-import { client } from '@tilework/opus'
 
 class App extends Component {
   render() {
-    client.setEndpoint("http://localhost:4000/")
 
     return (
       <div className="App">
