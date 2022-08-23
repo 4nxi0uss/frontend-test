d968ed33bffdded90b7ff99d0a20f47bf7e5896f
Artur Zaczek <artur200212@interia.pl>
2022-08-23 13:41:06 +0200

    creating ProductPage componet

---
+++ src/Components/ProductPage/ProductPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './ProductPage.scss'
+
+class ProductPage extends Component {
+    state = {}
+    render() {
+        return (<section>
+
+        </section>);
+    }
+}
+
+export default ProductPage;
