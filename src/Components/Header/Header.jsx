b1d2992fefd57572c6114aa05ecd3378c038342f
Artur Zaczek <artur200212@interia.pl>
2022-08-23 13:35:23 +0200

    added currencyMenu component and changing styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,51 +6,48 @@ import { apolloClient } from '../../Apollo/client';
 import { gql } from '@apollo/client';
 
 import { connect } from 'react-redux';
-
 import { changingCategory } from './headerSlice';
 
 import Logo from '../../img/logo-header.svg'
 import Cart from '../../img/cart-empty.svg'
-import ArrowDown from '../../img/arrow-down.svg'
+
+import CurrencyMenu from './Subcomponent/CurrencyMenu';
 
 class Header extends Component {
     state = {
         categories: [],
-        currencies: [],
     }
 
     componentDidMount() {
-        apolloClient.query({ query: gql`{categories {name}}` }).then((res) => (this.setState({ categories: res.data.categories })))
-        apolloClient.query({ query: gql`{currencies{label symbol}}` }).then((res) => (this.setState({ currencies: res.data.currencies })))
+        apolloClient
+            .query({ query: gql`{categories {name}}` })
+            .then((res) => (this.setState({ categories: res.data.categories })))
+            .catch(err => console.warn(err))
     }
 
     render() {
+
         const handleCategory = (e) => {
             this.props.changingCategory(e.target.innerHTML)
         }
 
         const headerCategorires = this.state.categories.map(({ name }) => (<li key={name} onClick={handleCategory}>{name}</li>))
-        const currenc = this.state.currencies.map(({ label, symbol }) => <option value={label}>{symbol} {label}</option>)
-        return (
-            <header className='header'>
-                <ul className='header__nav'>
-                    {headerCategorires}
-                </ul>
-                <img src={Logo} alt="header logo" className='header__logo' />
-                <div className='header__action'>
-                    <select name="curr" id="cur">
-                        {currenc}
-                    </select>
-                    <img src={Cart} alt="empty cart" className='header__action--cart' />
-                </div>
 
-            </header>
+        return (
+            <>
+                <header className='header'>
+                    <ul className='header__nav'>
+                        {headerCategorires}
+                    </ul>
+                    <img src={Logo} alt="header logo" className='header__logo' />
+                    <div className='header__action'>
+                        <CurrencyMenu />
+                        <img src={Cart} alt="empty cart" className='header__action--cart' />
+                    </div>
+                </header>
+            </>
         );
     }
 }
 
-const mapStateToPrps = (state) => ({
-    choosenCategories: state.category.category
-})
-
-export default connect(mapStateToPrps, { changingCategory })(Header);
+export default connect(null, { changingCategory })(Header);
