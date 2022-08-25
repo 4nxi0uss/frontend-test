c72cd2b488b4be63ec4fde0532ef7449152b444c
Artur Zaczek <artur200212@interia.pl>
2022-08-25 13:12:18 +0200

    added routes

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import './MainContent.scss'
 
 import CategoryPage from '../CategoryPage/CategoryPage';
+import { Route, Routes } from 'react-router-dom';
+import ProductPage from '../ProductPage/ProductPage';
 
 class MainContent extends Component {
 
     render() {
         return (<main className='main-content'>
-            <CategoryPage />
+            <Routes>
+                <Route path='/' element={<CategoryPage />} />
+                <Route path='/product-page' element={<ProductPage />} />
+            </Routes>
         </main>);
     }
 }
