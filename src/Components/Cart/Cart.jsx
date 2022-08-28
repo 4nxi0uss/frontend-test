e559880f24c0118852802010aa5411bf313940e7
Artur Zaczek <artur200212@interia.pl>
2022-08-29 00:40:56 +0200

    added all function for cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -8,34 +8,28 @@ import { connect } from 'react-redux';
 
 import { gql } from '@apollo/client';
 import { apolloClient } from '../../Apollo/client';
+import { changingCategory } from '../Header/headerSlice';
 
-let products;
 
 class Cart extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleChangeQuantity = this.handleChangeQuantity.bind(this)
+    }
 
     state = {
-        product: {},
-        totalQuantity: 0,
-        totalCost: 0,
+        productList: [],
         prices: []
     }
 
     componentDidMount() {
-        let cost = 0;
-        let productQuantity = 0;
+
+        this.props.changingCategory('')
 
         try {
-            const productAddedToCartList = JSON.parse(localStorage.getItem('products'))
+            this.setState({ productList: JSON.parse(localStorage.getItem('products')) })
 
-            products = productAddedToCartList?.map(({ id, attributes, quantity }) => <ProductRow key={JSON.stringify(attributes)} id={id} choosenAttributes={attributes} quantity={quantity} />)
-
-            productAddedToCartList?.forEach(({ quantity, prices }) => {
-                productQuantity += quantity;
-                cost += prices?.[this.props.currencyIndex]?.amount;
-
-            });
-            this.setState({ totalQuantity: productQuantity })
-            this.setState({ totalCost: cost })
         } catch (error) {
             console.warn(error)
         }
@@ -50,38 +44,54 @@ class Cart extends Component {
             .catch(err => console.warn(err))
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        let cost = 0;
-        let productQuantity = 0;
+    handleChangeQuantity(increment, index) {
+        let temporary = this.state?.productList
 
-        if (prevProps.currencyIndex === this.props.currencyIndex) return null
+        if (increment) {
+            temporary[index] = { ...temporary?.[index], quantity: temporary?.[index]?.quantity + 1 }
 
-        try {
-            const productAddedToCartList = JSON.parse(localStorage.getItem('products'))
+            localStorage.setItem('products', JSON.stringify(temporary))
+            this.setState({ productList: temporary })
+        }
 
-            productAddedToCartList?.forEach(({ quantity, prices }) => {
-                if (prevState.totalQuantity !== this.state.totalQuantity) {
-                    productQuantity += quantity;
-                }
-                if (prevProps.currencyIndex !== this.props.currencyIndex) {
-                    cost += prices?.[this.props.currencyIndex]?.amount;
-                }
-            })
-            this.setState({ totalQuantity: productQuantity })
-            this.setState({ totalCost: cost })
-        } catch (error) {
-            console.warn(error)
+        if (increment === false && temporary?.[index].quantity > 0) {
+            temporary[index] = { ...temporary?.[index], quantity: temporary?.[index]?.quantity - 1 }
+            console.log('decrese')
+            localStorage.setItem('products', JSON.stringify(temporary))
+            this.setState({ productList: temporary })
+        }
+
+        if (increment === false && temporary?.[index].quantity === 0) {
+            console.log('delete')
+            localStorage.setItem('products', JSON.stringify(temporary.slice(0, index)))
+            this.setState({ productList: temporary.slice(0, index) })
         }
 
     }
 
     render() {
 
+        const { currencyIndex } = this.props
+        const { productList } = this.state
+
+
+        const products = productList?.map(({ id, attributes, quantity }, index) => <ProductRow key={JSON.stringify(attributes)} id={id} choosenAttributes={attributes} index={index} quantity={quantity} fun={this.handleChangeQuantity} />)
+
         const getTax = (money) => (
             ((money / 100) * 21).toFixed(2)
         )
 
-        const { totalCost, totalQuantity } = this.state
+        const getTotalQuantity = () => {
+            let productQuantity = 0;
+            productList.forEach(({ quantity }) => { productQuantity += quantity })
+            return productQuantity
+        }
+
+        const getTotalCost = () => {
+            let cost = 0;
+            productList.forEach(({ prices, quantity }) => { cost += prices?.[currencyIndex].amount * quantity })
+            return cost.toFixed(2)
+        }
 
         return (
             <section className={`cart`}>
@@ -90,9 +100,9 @@ class Cart extends Component {
                 {products}
 
                 <div className={`cart__summary`}>
-                    <p className={`cart__summary__tax`}>tax 21%: <span>{getTax(totalCost)}</span></p>
-                    <p className={`cart__summary__quantity`}>Quantity: <span>{totalQuantity}</span></p>
-                    <p className={`cart__summary__total`}>Total: <span>{this.state?.prices?.[this.props.currencyIndex]?.symbol}{totalCost?.toFixed(2)}</span></p>
+                    <p className={`cart__summary__tax`}>tax 21%: <span className={`cart__summary__tax__amount`}>{this.state?.prices?.[this.props.currencyIndex]?.symbol}{getTax(getTotalCost())}</span></p>
+                    <p className={`cart__summary__quantity`}>Quantity: <span className={`cart__summary__quantity__amount`}>{getTotalQuantity()}</span></p>
+                    <p className={`cart__summary__total`}>Total: <span className={`cart__summary__total__amount`}>{this.state?.prices?.[this.props.currencyIndex]?.symbol}{getTotalCost()}</span></p>
                     <button className={`cart__summary__order-btn`}>order</button>
                 </div>
             </section>
@@ -104,4 +114,4 @@ const mapStateToPrps = (state) => ({
     currencyIndex: state.category.chosenCurrencies
 })
 
-export default connect(mapStateToPrps)(Cart);
+export default connect(mapStateToPrps, { changingCategory })(Cart);
