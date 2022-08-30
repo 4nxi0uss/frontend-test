3159b9e6f99dff1aff9fb8d89a770540c9ea51a0
Artur Zaczek <artur200212@interia.pl>
2022-08-30 15:13:22 +0200

    change handling quantity operation, from props to utilities

--- src/Components/Cart/Subcomponent/ProductRow/ProductRow.jsx
+++ src/Components/Cart/Subcomponent/ProductRow/ProductRow.jsx
@@ -5,9 +5,13 @@ import './ProductRow.scss'
 import { showAttributes } from '../../../ProductPage/ProductPage';
 
 import { apolloClient } from '../../../../Apollo/client';
-
 import { gql } from '@apollo/client';
+
 import { connect } from 'react-redux';
+import { changingCategory } from '../../../Header/headerSlice';
+import { decrementQuantity, incrementQuantity, removeProduct } from '../../../ProductPage/productSlice';
+
+import { handleChangeQuantity } from '../../../../Utilities/Utilities';
 
 const CATEGORY_QUERY = `query getProducts($id: String!) {
     product(id: $id) {
@@ -33,7 +37,7 @@ class ProductRow extends Component {
     }
 
     state = {
-        product: {},
+        productData: {},
         thumbnailsId: 0
     }
 
@@ -42,12 +46,12 @@ class ProductRow extends Component {
             .query({
                 query: gql`${CATEGORY_QUERY}`, variables: { id: this.props.id }
             })
-            .then((res) => (this.setState({ product: res.data.product })))
+            .then((res) => (this.setState({ productData: res.data.product })))
             .catch(err => console.warn(err))
     }
 
     handleChangethumbnail(increment) {
-        const quantityOfThumbnails = this.state?.product?.gallery?.length
+        const quantityOfThumbnails = this.state?.productData?.gallery?.length
 
         if (increment && quantityOfThumbnails > this.state.thumbnailsId + 1) {
 
@@ -61,9 +65,9 @@ class ProductRow extends Component {
 
     render() {
 
-        const { currencyIndex, quantity, choosenAttributes, fun, index } = this.props
+        const { currencyIndex, quantity, choosenAttributes, index } = this.props
 
-        const { gallery, brand, name, prices, attributes } = this.state.product
+        const { gallery, brand, name, prices, attributes } = this.state.productData
 
         const { thumbnailsId } = this.state
 
@@ -77,13 +81,13 @@ class ProductRow extends Component {
                 </div>
                 <div className={`product__div`}>
                     <div className={`product__div__increase-amount`}>
-                        <button className={`product__div__increase-amount__btn`} onClick={() => { fun(true, index) }}>+</button>
+                        <button className={`product__div__increase-amount__btn`} onClick={() => { handleChangeQuantity(true, index, this.props) }}>+</button>
                         <p className={`product__div__increase-amount__product-amount`}>{quantity}</p>
-                        <button className={`product__div__increase-amount__btn`} onClick={() => { fun(false, index) }}>-</button>
+                        <button className={`product__div__increase-amount__btn`} onClick={() => { handleChangeQuantity(false, index, this.props) }}>-</button>
                     </div>
                     <div className={`product__div__div-img`}>
                         <img src={gallery?.[thumbnailsId]} alt="" className={`product__div__div-img__img`} />
-                        <div className={`product__div__div-img__img-switch`} style={({ display: `${gallery?.length === 1 ? 'none' : 'block'}` })} >
+                        <div className={`product__div__div-img__img-switch`} style={({ display: `${gallery?.length === 1 ? 'none' : 'flex'}` })} >
                             <button className={`product__div__div-img__img-switch__btn`} onClick={() => { this.handleChangethumbnail(false) }}>{`<`}</button>
                             <button className={`product__div__div-img__img-switch__btn`} onClick={() => { this.handleChangethumbnail(true) }}>{`>`}</button>
                         </div>
@@ -95,7 +99,8 @@ class ProductRow extends Component {
 }
 
 const mapStateToPrps = (state) => ({
-    currencyIndex: state.category.chosenCurrencies
+    currencyIndex: state.category.chosenCurrencies,
+    productList: state.product.productList
 })
 
-export default connect(mapStateToPrps)(ProductRow);
+export default connect(mapStateToPrps, { changingCategory, incrementQuantity, decrementQuantity, removeProduct })(ProductRow);
