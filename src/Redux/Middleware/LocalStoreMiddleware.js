b688ef2469b70c5d0478c24984803a2bbcfca200
Artur Zaczek <artur200212@interia.pl>
2022-08-30 15:01:50 +0200

    added new condition to middleware

--- src/Redux/Middleware/LocalStoreMiddleware.js
+++ src/Redux/Middleware/LocalStoreMiddleware.js
@@ -5,6 +5,12 @@ export const localSotrageMiddleware = (store) => (next) => ({ type, payload }) =
     if (type.startsWith('product')) {
         localStorage.setItem('products', JSON.stringify(store.getState().product.productList))
     }
+    if (type.startsWith('cart')) {
+        localStorage.setItem('cart', store.getState().cart.cartId)
+    }
+    if (type === ('header/changingCurrencies')) {
+        localStorage.setItem('currencieIndex', JSON.stringify(store.getState().category.chosenCurrencies))
+    }
 
     return result;
 }
--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+
+import { localSotrageMiddleware } from '../Middleware/LocalStoreMiddleware'
+
 import headerSlice from '../../Components/Header/headerSlice'
 import cartSlice from '../../Components/CategoryPage/cartSlice'
 import productSlice from '../../Components/ProductPage/productSlice'
-import { localSotrageMiddleware } from '../Middleware/LocalStoreMiddleware'
 
 export const store = configureStore({
     reducer: {
