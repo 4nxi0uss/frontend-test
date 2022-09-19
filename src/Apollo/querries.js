4a4dd2556c7724c29d6e7f704f6fc5e1a8695b47
Artur Zaczek <artur200212@interia.pl>
2022-09-19 13:30:18 +0200

    added query file

---
+++ src/Apollo/querries.js
@@ -0,0 +1,98 @@
+export const PRODUCT_SHORT_QUERY = `query getProduct($id: String!) {
+  product(id: $id) {
+    id
+    name
+    gallery
+    attributes {
+      id
+      name
+      type
+      items {
+        displayValue
+        value
+        id
+      }
+    }
+    prices {
+      currency {
+        symbol
+      }
+      amount
+    }
+    brand
+  }
+}`
+
+export const PRODUCT_QUERY = `query getProduct($id: String!) {
+  product(id: $id) {
+    id
+    name
+    inStock
+    gallery
+    description
+    category
+    attributes {
+      id
+      name
+      type
+      items {
+        displayValue
+        value
+        id
+      }
+    }
+    prices {
+      currency {
+        label
+        symbol
+      }
+      amount
+    }
+    brand
+  }
+}`
+
+export const CURRENCIES_QUERY = `{
+    currencies {
+      label
+      symbol
+    }
+  }
+  `
+
+export const CATEGORY_PRODUCT_QUERY = `query getCategoryProducts($cat: String!) {
+  category(input: { title: $category }) {
+    products {
+      id
+      name
+      inStock
+      gallery
+      description
+      category
+      brand
+      prices {
+        currency {
+          label
+          symbol
+        }
+        amount
+      }
+      attributes {
+        id
+        name
+        type
+        items {
+          displayValue
+          value
+          id
+        }
+      }
+    }
+  }
+}`
+
+export const CATEGORY_QUERY = `{
+  categories {
+    name
+  }
+}`
